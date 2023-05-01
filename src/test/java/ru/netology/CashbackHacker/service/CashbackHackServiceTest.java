194cb7444d2dbe640297e6342f66166f54b00aed
Olga Em <olga.em020@gmail.com>
2023-05-01 13:09:53 +0700

    Dependence testng

--- src/test/java/ru/netology/CashbackHacker/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHacker/service/CashbackHackServiceTest.java
@@ -1,7 +1,47 @@
 package ru.netology.CashbackHacker.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
-class CashbackHackServiceTest {
+//import static org.junit.jupiter.api.Assertions.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+
+    public void PurchaseUnder1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void Purchase1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void PurchaseOver1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
 
 }
