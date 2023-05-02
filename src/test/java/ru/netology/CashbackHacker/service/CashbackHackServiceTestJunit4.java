16c80e3830f43d15f415c4d2dda24fd0627015c0
Olga Em <olga.em020@gmail.com>
2023-05-02 18:15:52 +0700

    Add a test class

---
+++ src/test/java/ru/netology/CashbackHacker/service/CashbackHackServiceTestJunit4.java
@@ -0,0 +1,45 @@
+package ru.netology.CashbackHacker.service;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTestJunit4 {
+
+    @Test
+
+    public void PurchaseUnder1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void Purchase1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void PurchaseOver1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+}
